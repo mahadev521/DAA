f4a82eb2830caa82fe4dabe4b33c5db8cbf91cf7
Srimanth Mahadev Kancharla <78967903+mahadev521@users.noreply.github.com>
2021-11-05 10:43:49 +0530

    Update HR_coinchange.py

--- HR_coinchange.py
+++ HR_coinchange.py
@@ -1,13 +1,10 @@
-def getWays(n, c):
-    dp = [1]+[0]*n
-    for coin in c:
-        for i in range(coin, n+1):
-            dp[i] += dp[i-coin]
-    return dp[n]
-    
 n,m=[eval(x) for x in input().split()]
 c=[eval(x) for x in input().split()]
-print(getWays(n, c))
+dp = [1]+[0]*n
+for coin in c:
+    for i in range(coin, n+1):
+        dp[i] += dp[i-coin]
+print(dp[n])
 
 # ip:
 # 4 3
