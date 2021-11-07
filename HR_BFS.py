ee200a3d2d3127009985d54d4c2fa54570300f01
Srimanth Mahadev Kancharla <78967903+mahadev521@users.noreply.github.com>
2021-11-07 16:19:12 +0530

    Add files via upload

---
+++ HR_BFS.py
@@ -0,0 +1,60 @@
+import queue as Qu
+def BFS(s):
+    q = Qu.Queue()
+    q.put(s)
+    visited[s] = True
+    dist[s] = 0
+
+    while not q.empty():
+        u = q.get()
+        for v in graph[u]:
+            if not visited[v]:
+                visited[v] = True
+                q.put(v)
+                dist[v] = dist[u] + 1
+
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+
+    graph = [[] for i in range(n)]
+    visited = [False]*n
+    dist = [-1]*n
+
+    for _ in range(m):
+        u, v = map(lambda x: int(x) - 1, input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+    s = int(input()) - 1
+
+    BFS(s)
+    for i in range(n):
+        if i == s:
+            continue
+        print(dist[i]*6 if dist[i] != -1 else '-1', end = ' ')
+    print()
+
+# Input:
+# 2
+# 4 2
+# 1 2
+# 1 3
+# 1
+# 3 1
+# 2 3
+# 2
+
+# Output:
+# 6 6 -1
+# -1 6
+
+# Input2:
+# 1
+# 5 3
+# 1 2
+# 1 3
+# 3 4
+# 1
+
+# Output2:
+# 6 6 12 -1
---
+++ HR_krushkal.py
@@ -0,0 +1,41 @@
+def Kruskal(V,mat):
+    mstsum=0
+    mat=sorted(mat,key=lambda x:x[2])
+    while (len(mat)>0):
+        if mat[0][0] not in V[mat[0][1]-1] or mat[0][1] not in V[mat[0][0]-1]:
+            V[mat[0][0]-1]=V[mat[0][0]-1] | V[mat[0][1]-1]
+            V[mat[0][1]-1]=V[mat[0][0]-1]
+            for i in V[mat[0][0]-1]:
+                V[i-1]=V[mat[0][0]-1]
+            mstsum+=mat[0][2]
+        mat.pop(0)
+    print(mstsum)
+
+N,M=map(int,input().split())
+mat,V=[],[]
+[mat.append(list(map(int,input().split()))) for _ in range(M)]
+[V.append(set([i])) for i in range(1,N+1)]
+Kruskal(V,mat)
+
+# Input:
+# 4 6
+# 1 2 5
+# 1 3 3
+# 4 1 6
+# 2 4 7
+# 3 2 4
+# 3 4 5
+# Output:
+# 12
+
+# Input:
+# 5 7
+# 1 2 20
+# 1 3 50
+# 1 4 70
+# 1 5 90
+# 2 3 30
+# 3 4 40
+# 4 5 60
+# Output:
+# 150
