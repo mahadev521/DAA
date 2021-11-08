7304a18c95b7e28436a231800309511f11ec6792
Srimanth Mahadev Kancharla <78967903+mahadev521@users.noreply.github.com>
2021-11-08 18:31:35 +0530

    Update kthsmall.py

--- kthsmall.py
+++ kthsmall.py
@@ -2,23 +2,23 @@ def merge_sort(arr):
     if len(arr) <= 1:
         return arr
     mid = len(arr) // 2
-    left, right = merge_sort(arr[:mid]), merge_sort(arr[mid:])
-    return merge(left, right, arr.copy())
+    l, r = merge_sort(arr[:mid]), merge_sort(arr[mid:])
+    return merge(l, r, arr.copy())
 
 
-def merge(left, right, merged):
-    left_cursor, right_cursor = 0, 0
-    while left_cursor < len(left) and right_cursor < len(right):
-        if left[left_cursor] <= right[right_cursor]:
-            merged[left_cursor+right_cursor]=left[left_cursor]
-            left_cursor += 1
+def merge(l, r, merged):
+    left, right = 0, 0
+    while left < len(l) and right < len(r):
+        if l[left] <= r[right]:
+            merged[left+right]=l[left]
+            left += 1
         else:
-            merged[left_cursor + right_cursor] = right[right_cursor]
-            right_cursor += 1
-    for left_cursor in range(left_cursor, len(left)):
-        merged[left_cursor + right_cursor] = left[left_cursor]
-    for right_cursor in range(right_cursor, len(right)):
-        merged[left_cursor + right_cursor] = right[right_cursor]
+            merged[left + right] = r[right]
+            right += 1
+    for left in range(left, len(l)):
+        merged[left + right] = l[left]
+    for right in range(right, len(r)):
+        merged[left + right] = r[right]
     return merged
 
 n=int(input('enter number of elements: '))
