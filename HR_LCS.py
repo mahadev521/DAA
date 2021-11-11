68e64271422cb9fc80be3f714e28b71d09002e0b
Srimanth Mahadev Kancharla <78967903+mahadev521@users.noreply.github.com>
2021-11-11 18:02:48 +0530

    Update HR_LCS.py

--- HR_LCS.py
+++ HR_LCS.py
@@ -4,14 +4,11 @@ y = [int(x) for x in input().split()]
 dp = [[0 for i in range(m + 1)] for j in range(n + 1)]
 for i in range(n + 1):
     for j in range(m + 1):
-        if i == 0 or j == 0:
-            dp[i][j] = 0
+        if i == 0 or j == 0: dp[i][j] = 0
 for i in range(1, n + 1):
     for j in range(1, m + 1):
-        if x[i - 1] == y[j - 1]:
-            dp[i][j] = 1 + dp[i - 1][j - 1]
-        else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        if x[i - 1] == y[j - 1]: dp[i][j] = 1 + dp[i - 1][j - 1]
+        else: dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 i, j, v = n, m, []
 while i > 0 and j > 0:
     if x[i - 1] == y[j - 1]:
@@ -19,10 +16,8 @@ while i > 0 and j > 0:
         i -= 1
         j -= 1
     else:
-        if dp[i - 1][j] > dp[i][j - 1]:
-            i -= 1
-        else :
-            j -= 1
+        if dp[i - 1][j] > dp[i][j - 1]: i -= 1
+        else : j -= 1
 print(*reversed(v))
 
 
