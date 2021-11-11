db73122a9a505df8a1bc52b1acf6e8054e208d27
Srimanth Mahadev Kancharla <78967903+mahadev521@users.noreply.github.com>
2021-11-11 19:21:35 +0530

    Update nQueensII.py

--- nQueensII.py
+++ nQueensII.py
@@ -1,31 +1,23 @@
 #this code donot use numpy and gives all possible solutions for n queens problem
-def display(mat):
-    for r in mat:
-        print(*r)
-    print()
-
 def isSafe(mat, r, c):
     for i in range(r):
-        if mat[i][c] == 1:
-            return False
+        if mat[i][c] == 1: return False
     i, j = r, c
     while i >= 0 and j >= 0:
-        if mat[i][j] == 1:
-            return False
-        i = i - 1
-        j = j - 1
+        if mat[i][j] == 1: return False
+        i -= 1
+        j -= 1
     i, j = r, c
     while i >= 0 and j < len(mat):
-        if mat[i][j] == 1:
-            return False
-        i = i - 1
-        j = j + 1
+        if mat[i][j] == 1: return False
+        i -= 1
+        j += 1
     return True
- 
  
 def nQueen(mat, r):
     if r == len(mat):
-        display(mat)
+        for r in mat: print(*r)
+        print()
         return
     for i in range(len(mat)):
         if isSafe(mat, r, i):
@@ -34,8 +26,7 @@ def nQueen(mat, r):
             mat[r][i] = 0
  
 n = int(input('enter number of queens: '))
-if n<4:
-    print('there is no such arrangement')
+if n<4: print('there is no such arrangement')
 else:
-    mat = [[0 for x in range(n)] for y in range(n)]
+    mat = [[0 for _ in range(n)] for _ in range(n)]
     nQueen(mat, 0)
