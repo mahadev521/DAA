1d909fd1da28ad0dde2b0f15e23abd43bc6a3693
Srimanth Mahadev Kancharla <78967903+mahadev521@users.noreply.github.com>
2021-11-11 18:04:18 +0530

    Update HR_coinchange.py

--- HR_coinchange.py
+++ HR_coinchange.py
@@ -2,8 +2,7 @@ n,m=[eval(x) for x in input().split()]
 c=[eval(x) for x in input().split()]
 dp = [1]+[0]*n
 for i in c:
-    for j in range(i, n+1):
-        dp[j] += dp[j-i]
+    for j in range(i, n+1): dp[j] += dp[j-i]
 print(dp[n])
 
 # ip:
