bc6e50b826e9c7de952326646642938874bc9ae2
Srimanth Mahadev Kancharla <78967903+mahadev521@users.noreply.github.com>
2021-11-12 19:41:26 +0530

    Update knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -3,12 +3,12 @@ val=[-1*eval(x) for x in input('enter values/profits of items: ').split()]
 wt=[eval(x) for x in input('enter weights of items: ').split()]
 W=int(input('enter knapsack capacity: '+'\n'))
 knap=[1]*n
-w=lb=up=0
+w=lb=0
 for j in range(n):
     if w+wt[j]<=W:
         w+=wt[j]
         lb+=val[j]
-upper=up=lb
+upper=lb
 lb+=((val[j]/wt[j])*(W-w))
 for i in range(n):
     lb1=up1=0
@@ -19,7 +19,6 @@ for i in range(n):
                 lb1+=val[j]
     up1=lb1
     lb1+=((val[j]/wt[j])*(W-w1))
-    print(up,lb,up1,lb1)
     if upper<lb1: 
         continue
     elif upper>up1:
@@ -29,9 +28,9 @@ for i in range(n):
         knap[i]=0
         lb=lb1
     elif lb1==lb:
-        if up>up1:
+        if upper>up1:
             knap[i]=0
-            up=upper=up1
+            upper=up1
             lb=lb1
 print(-1*upper)
 
