a0302a0b90d58fe7e476358729d365596dc26db4
Srimanth Mahadev Kancharla <78967903+mahadev521@users.noreply.github.com>
2021-11-13 08:45:04 +0530

    Update maxmin_DAC.py

--- maxmin_DAC.py
+++ maxmin_DAC.py
@@ -1,11 +1,9 @@
 '''1. Find the min-max of list of elements using divide and conquer strategy.'''
-
 def maxmin(a, x, y):
-    maxi, mini = 0, 0
-    d = e = f = [0, 0]
+    maxi, mini =d = e = [0, 0]
     if y-x <= 1:
-        f[0] = max(a[int(x)], a[int(y)])
-        f[1] = min(a[int(x)], a[int(y)])
+        d[0] = max(a[int(x)], a[int(y)])
+        d[1] = min(a[int(x)], a[int(y)])
     else:
         d = maxmin(a, x, (x + y)/2)
         e = maxmin(a, ((x + y)/2)+1, y)
@@ -15,5 +13,4 @@ def maxmin(a, x, y):
 
 x = int(input('enter number of elements: '))
 y = [eval(x) for x in input('enter elements ').split()]
-o = maxmin(y, 0, len(y)-1)
-print('The maximum and minimum elements in given elements are: ', *o)
+print('The maximum and minimum elements in given numbers are: ', *maxmin(y, 0, len(y)-1))
