18c582df6987836cbc4ac21e85aa77cf1e03d80d
Srimanth Mahadev Kancharla <78967903+mahadev521@users.noreply.github.com>
2021-11-13 11:02:40 +0530

    Update floydalgo.py

--- floydalgo.py
+++ floydalgo.py
@@ -6,14 +6,14 @@ def paths(path, v, u, route):
     paths(path, v, path[v][u], route)
     route.append(path[v][u])
 
-def display(path, n):
+def display(path, n, cost):
     for v in range(n):
         for u in range(n):
             if u != v and path[v][u] != -1:
                 route = [v]
                 paths(path, v, u, route)
                 route.append(u)
-                print(f'shortest path from {v} to {u}: ', *route)
+                print(f'with distance: {cost[v][u]}\tshortest path from {v} to {u} is ',*route)
  
 def floyd(G,n):
     if not G: return
@@ -30,7 +30,7 @@ def floyd(G,n):
                 if cost[v][k] != float('inf') and cost[k][u] != float('inf') and (cost[v][k] + cost[k][u] < cost[v][u]):
                     cost[v][u] = cost[v][k] + cost[k][u]
                     path[v][u] = path[k][u]
-    display(path, n)
+    display(path, n, cost)
  
 ver = int(input('enter number of vertices of graph: '))
 inf = float('inf')
@@ -40,7 +40,7 @@ for i in range(ver):
         if G[i][j]==0 and i!=j: G[i][j]=inf
 floyd(G,ver)
 
-# ip:
+# # ip:
 # 9
 # 0 4 0 0 0 0 0 8 0 
 # 4 0 8 0 0 0 0 11 0
@@ -52,56 +52,75 @@ floyd(G,ver)
 # 8 11 0 0 0 0 1 0 7
 # 0 0 2 0 0 0 6 7 0
 # op:
-# shortest path from 0 to 1:  0 1
-# shortest path from 0 to 2:  0 1 2
-# shortest path from 0 to 3:  0 1 2 3
-# shortest path from 0 to 4:  0 7 6 5 4
-# shortest path from 0 to 5:  0 7 6 5
-# shortest path from 0 to 6:  0 7 6
-# shortest path from 0 to 7:  0 7
-# shortest path from 0 to 8:  0 1 2 8
-# shortest path from 1 to 0:  1 0
-# shortest path from 1 to 2:  1 2
-# shortest path from 1 to 3:  1 2 3
-# shortest path from 1 to 4:  1 2 5 4
-# shortest path from 1 to 5:  1 2 5
-# shortest path from 1 to 6:  1 7 6
-# shortest path from 1 to 7:  1 7
-# shortest path from 1 to 8:  1 2 8
-# shortest path from 2 to 0:  2 1 0
-# shortest path from 2 to 1:  2 1
-# shortest path from 2 to 3:  2 3
-# shortest path from 2 to 4:  2 5 4
-# shortest path from 2 to 5:  2 5
-# shortest path from 2 to 6:  2 5 6
-# shortest path from 2 to 7:  2 5 6 7
-# shortest path from 2 to 8:  2 8
-# shortest path from 3 to 0:  3 2 1 0
-# shortest path from 3 to 1:  3 2 1
-# shortest path from 3 to 2:  3 2
-# shortest path from 3 to 4:  3 4
-# shortest path from 3 to 5:  3 2 5
-# shortest path from 3 to 6:  3 2 5 6
-# shortest path from 3 to 7:  3 2 5 6 7
-# shortest path from 3 to 8:  3 2 8
-# shortest path from 4 to 0:  4 5 6 7 0
-# shortest path from 4 to 1:  4 5 2 1
-# shortest path from 4 to 2:  4 5 2
-# shortest path from 4 to 3:  4 3
-# shortest path from 4 to 5:  4 5
-# shortest path from 4 to 6:  4 5 6
-# shortest path from 4 to 7:  4 5 6 7
-# shortest path from 7 to 2:  7 6 5 2
-# shortest path from 7 to 3:  7 6 5 2 3
-# shortest path from 7 to 4:  7 6 5 4
-# shortest path from 7 to 5:  7 6 5
-# shortest path from 7 to 6:  7 6
-# shortest path from 7 to 8:  7 8
-# shortest path from 8 to 0:  8 2 1 0
-# shortest path from 8 to 1:  8 2 1
-# shortest path from 8 to 2:  8 2
-# shortest path from 8 to 3:  8 2 3
-# shortest path from 8 to 4:  8 2 5 4
-# shortest path from 8 to 5:  8 2 5
-# shortest path from 8 to 6:  8 6
-# shortest path from 8 to 7:  8 7
+# with distance: 4        shortest path from 0 to 1 is  0 1
+# with distance: 12       shortest path from 0 to 2 is  0 1 2
+# with distance: 19       shortest path from 0 to 3 is  0 1 2 3
+# with distance: 21       shortest path from 0 to 4 is  0 7 6 5 4
+# with distance: 11       shortest path from 0 to 5 is  0 7 6 5
+# with distance: 9        shortest path from 0 to 6 is  0 7 6
+# with distance: 8        shortest path from 0 to 7 is  0 7
+# with distance: 14       shortest path from 0 to 8 is  0 1 2 8
+# with distance: 4        shortest path from 1 to 0 is  1 0
+# with distance: 8        shortest path from 1 to 2 is  1 2
+# with distance: 15       shortest path from 1 to 3 is  1 2 3
+# with distance: 22       shortest path from 1 to 4 is  1 2 5 4
+# with distance: 12       shortest path from 1 to 5 is  1 2 5
+# with distance: 12       shortest path from 1 to 6 is  1 7 6
+# with distance: 11       shortest path from 1 to 7 is  1 7
+# with distance: 10       shortest path from 1 to 8 is  1 2 8
+# with distance: 12       shortest path from 2 to 0 is  2 1 0
+# with distance: 8        shortest path from 2 to 1 is  2 1
+# with distance: 7        shortest path from 2 to 3 is  2 3
+# with distance: 14       shortest path from 2 to 4 is  2 5 4
+# with distance: 4        shortest path from 2 to 5 is  2 5
+# with distance: 6        shortest path from 2 to 6 is  2 5 6
+# with distance: 7        shortest path from 2 to 7 is  2 5 6 7
+# with distance: 2        shortest path from 2 to 8 is  2 8
+# with distance: 19       shortest path from 3 to 0 is  3 2 1 0
+# with distance: 15       shortest path from 3 to 1 is  3 2 1
+# with distance: 7        shortest path from 3 to 2 is  3 2
+# with distance: 9        shortest path from 3 to 4 is  3 4
+# with distance: 11       shortest path from 3 to 5 is  3 2 5
+# with distance: 13       shortest path from 3 to 6 is  3 2 5 6
+# with distance: 14       shortest path from 3 to 7 is  3 2 5 6 7
+# with distance: 9        shortest path from 3 to 8 is  3 2 8
+# with distance: 21       shortest path from 4 to 0 is  4 5 6 7 0
+# with distance: 22       shortest path from 4 to 1 is  4 5 2 1
+# with distance: 14       shortest path from 4 to 2 is  4 5 2
+# with distance: 9        shortest path from 4 to 3 is  4 3
+# with distance: 10       shortest path from 4 to 5 is  4 5
+# with distance: 12       shortest path from 4 to 6 is  4 5 6
+# with distance: 13       shortest path from 4 to 7 is  4 5 6 7
+# with distance: 16       shortest path from 4 to 8 is  4 5 2 8
+# with distance: 11       shortest path from 5 to 0 is  5 6 7 0
+# with distance: 12       shortest path from 5 to 1 is  5 2 1
+# with distance: 4        shortest path from 5 to 2 is  5 2
+# with distance: 11       shortest path from 5 to 3 is  5 2 3
+# with distance: 10       shortest path from 5 to 4 is  5 4
+# with distance: 2        shortest path from 5 to 6 is  5 6
+# with distance: 3        shortest path from 5 to 7 is  5 6 7
+# with distance: 6        shortest path from 5 to 8 is  5 2 8
+# with distance: 9        shortest path from 6 to 0 is  6 7 0
+# with distance: 12       shortest path from 6 to 1 is  6 7 1
+# with distance: 6        shortest path from 6 to 2 is  6 5 2
+# with distance: 13       shortest path from 6 to 3 is  6 5 2 3
+# with distance: 12       shortest path from 6 to 4 is  6 5 4
+# with distance: 2        shortest path from 6 to 5 is  6 5
+# with distance: 1        shortest path from 6 to 7 is  6 7
+# with distance: 6        shortest path from 6 to 8 is  6 8
+# with distance: 8        shortest path from 7 to 0 is  7 0
+# with distance: 11       shortest path from 7 to 1 is  7 1
+# with distance: 7        shortest path from 7 to 2 is  7 6 5 2
+# with distance: 14       shortest path from 7 to 3 is  7 6 5 2 3
+# with distance: 13       shortest path from 7 to 4 is  7 6 5 4
+# with distance: 3        shortest path from 7 to 5 is  7 6 5
+# with distance: 1        shortest path from 7 to 6 is  7 6
+# with distance: 7        shortest path from 7 to 8 is  7 8
+# with distance: 14       shortest path from 8 to 0 is  8 2 1 0
+# with distance: 10       shortest path from 8 to 1 is  8 2 1
+# with distance: 2        shortest path from 8 to 2 is  8 2
+# with distance: 9        shortest path from 8 to 3 is  8 2 3
+# with distance: 16       shortest path from 8 to 4 is  8 2 5 4
+# with distance: 6        shortest path from 8 to 5 is  8 2 5
+# with distance: 6        shortest path from 8 to 6 is  8 6
+# with distance: 7        shortest path from 8 to 7 is  8 7
