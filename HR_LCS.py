c67535bbf0458fc7fbe05199e7e101e75b7a3c0f
Srimanth Mahadev Kancharla <78967903+mahadev521@users.noreply.github.com>
2021-11-14 19:02:10 +0530

    Update HR_LCS.py

--- HR_LCS.py
+++ HR_LCS.py
@@ -2,9 +2,6 @@ n,m =list(map(int,input().split()))
 x = [int(x) for x in input().split()]
 y = [int(x) for x in input().split()]
 dp = [[0 for i in range(m + 1)] for j in range(n + 1)]
-for i in range(n + 1):
-    for j in range(m + 1):
-        if i == 0 or j == 0: dp[i][j] = 0
 for i in range(1, n + 1):
     for j in range(1, m + 1):
         if x[i - 1] == y[j - 1]: dp[i][j] = 1 + dp[i - 1][j - 1]
